25dff5a9aa512ef678f254cf3184b34791f2163a
Avatharian <jason@valdezak.net>
2014-11-24 08:22:53 -0900

    Moved the resources directory out.
    Having that stuff in the repo was silly and not what git is meant for.

--- resourcescripts/rtrouton/active_directory_check/adcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if ping -c 1 internal.ip.address1 || ping -c 1 internal.ip.address2
-then
-     dscl /Search -append / CSPSearchPath "/Active Directory/domain.com"
-else
-     dscl /Search -delete / CSPSearchPath "/Active Directory/domain.com"
-fi
--- resourcescripts/rtrouton/disable_gatekeeper/script/disable_gatekeeper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Turn off Gatekeeper
-
-spctl --master-disable
