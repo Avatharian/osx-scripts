5e5e69502f87b912fe379d1b286c76c8c97f6b0a
Avatharian <jason@valdezak.net>
2014-12-22 10:10:23 -0900

    Comments and removed extraneous lines
    Added comments to the get name script. Removed a bunch of lines that
    apparently didn’t actually do anything, and also removed the file copy
    process and streamlined the process of copying the computer name to the
    local hard drive.

--- scripts/All/get-set name/getname.sh
+++ scripts/All/get-set name/getname.sh
@@ -10,28 +10,39 @@
 # $KACE_SYSTEM_DRIVE_NAME/Users/Shared/{MAC}
 #
 ##################################################################
+
+
+#Retrieves the system's serial number as variable $SERIAL
 SERIAL=`/usr/sbin/system_profiler SPHardwareDataType | awk '/Serial/ {print $4}'`
 
+#Sets the TEMP_PATH variable to the fodler on the K2000 where the computer name will be temporarily stored
 TEMP_PATH="/opt/kace/petemp/${SERIAL}"
 
-IFS=$'\n'
-volume=`mount | grep -m 1 disk0`
-zeroMount=`echo "$volume" | awk  '{  print substr($0,index($0, "on ")+3,index($0, "(")-index($0, "on ")-4) }'`
-
+#setting the prefPath variable to target the machines original preferences.plist on it's local drive, in preparation to
+#retrieve the old computer name
 prefPath=${KACE_SYSTEM_DRIVE_PATH}/Library/Preferences/SystemConfiguration/preferences.plist
 
+#Reads the computers original name using plist buddy, and setting the tComputerName variable with it
 tComputerName=`/usr/libexec/PlistBuddy -c "Print System:System:ComputerName" $prefPath` 
+
+#Ok. This will set the ComputerName variable to equal the tComputername variable IF the tComputername contains "Not Exist".
+#I think. Otherwise this line does nothing, and basically sets the computerName varible to Null.
 computerName=`echo $tComputerName | awk '{ print index($0,"Not Exist") }'`
+
+#sets the fixMe variable to concatenate "FIXME-" + the Serial Number of the machine
 fixMe="FIXME-$SERIAL"
+
+#If the $computerName varibale is equal to x0 (in other words, did the previous step set it to Nthing), 
+#that means that a name was in fact found, and the $computerName variable should be set to it.
+#Otherwise, fallback on the FIXME+Serial Number name
 if [x$computerName == x0 ]; then
    computerName=$tComputerName
 else 
    computerName="$fixMe"
 fi
 
-echo $computerName > "${TEMP_PATH}"
+#Echoes the $computerName variable to the local disk, where it will be read post-imaging and applied to the machine.
+echo $computerName > "${KACE_SYSTEM_DRIVE_PATH}/Users/Shared/${SERIAL}"
 
-cp "/opt/kace/petemp/${$SERIAL}" "${KACE_SYSTEM_DRIVE_PATH}/Users/Shared/${$SERIAL}"
-rm -f "/opt/kace/petemp/${$SERIAL}"
 
 exit 0
