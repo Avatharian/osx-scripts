e442c85a4591f1acf2f4e082c040f4a94bcaf8d3
Avatharian <jason@valdezak.net>
2014-12-23 10:39:28 -0900

    Added the Kbox Postinstall task converter

---
+++ scripts/All/KboxPostinstallConverter/pushbutan/pushbutan.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "PUSH BUTAN, SHOT WEB"
+#delete the loginhook
+/usr/bin/defaults delete com.apple.loginwindow LoginHook
+#launch LoginLog
+/var/kace/k2000/LoginLog.app/Contents/MacOS/LoginLog -logfile /tmp/outfile
+#launch the postinstall task engine
+sh /var/kace/k2000/system_image*.sh
---
+++ scripts/All/KboxPostinstallConverter/taskengineconvert.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#This script converts the K2000's dumb loginhook implementation into a proper LaunchD.
+
+#Install the launchd and the script which launches the K2000 task engine and the LoginLog.app
+cp ./cc.valdez.taskengine.plist "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.taskengine.plist"
+chown root "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.taskengine.plist"
+chmod 644 "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.taskengine.plist"
+
+cp ./cc.valdez.LoginLog.plist "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.LoginLog.plist"
+chown root "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.LoginLog.plist"
+chmod 644 "$KACE_SYSTEM_DRIVE_PATH/Library/LaunchDaemons/cc.valdez.LoginLog.plist"
+
+
+cp ./pushbutan.sh "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/pushbutan.sh"
+chown root "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/pushbutan.sh"
+chmod 744 "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/pushbutan.sh"
+
+cp -r ./LoginLog.app "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/LoginLog.app"
+chown -R root "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/LoginLog.app"
+chmod -R 744 "$KACE_SYSTEM_DRIVE_PATH/Library/Scripts/LoginLog.app"
