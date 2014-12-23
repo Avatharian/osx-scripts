d4ec67b7f03755db3f47ada44d6b2f7a577a4d58
Avatharian <jason@valdezak.net>
2014-12-23 11:06:45 -0900

    Created task engine plist and corrected error

--- scripts/All/KboxPostinstallConverter/pushbutan/pushbutan.sh
+++ scripts/All/KboxPostinstallConverter/pushbutan/pushbutan.sh
@@ -3,6 +3,6 @@ echo "PUSH BUTAN, SHOT WEB"
 #delete the loginhook
 /usr/bin/defaults delete com.apple.loginwindow LoginHook
 #launch LoginLog
-/var/kace/k2000/LoginLog.app/Contents/MacOS/LoginLog -logfile /tmp/outfile
+Library/Scripts/LoginLog.app/Contents/MacOS/LoginLog -logfile /tmp/outfile
 #launch the postinstall task engine
 sh /var/kace/k2000/system_image*.sh
