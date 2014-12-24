3cf939b8f5c3a7e18fab14d638b84d8001be8515
Avatharian <jason@valdezak.net>
2014-12-24 09:49:49 -0900

    Added loginlog launchd and controls.

--- scripts/All/KboxPostinstallConverter/pushbutan.sh
+++ scripts/All/KboxPostinstallConverter/pushbutan.sh
@@ -2,7 +2,5 @@
 echo "PUSH BUTAN, SHOT WEB"
 #delete the loginhook
 /usr/bin/defaults delete com.apple.loginwindow LoginHook
-#launch LoginLog
-Library/Scripts/LoginLog.app/Contents/MacOS/LoginLog -logfile /tmp/outfile &
 #launch the postinstall task engine
 sh /var/kace/k2000/system_image*.sh
