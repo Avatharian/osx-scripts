9b8c1b4623fb8242860149ccae1f60926087f7fb
Avatharian <jason@valdezak.net>
2016-02-09 15:13:37 -0900

    Cleanup

--- scripts/All/KboxPostinstallConverter/pushbutan.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "PUSH BUTAN, SHOT WEB"
-#delete the loginhook
-/usr/bin/defaults delete com.apple.loginwindow LoginHook
-#launch the postinstall task engine
-sh /var/kace/k2000/system_image*.sh
